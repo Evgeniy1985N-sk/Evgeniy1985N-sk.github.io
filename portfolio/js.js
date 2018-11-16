c55b7e0b8f9ccb9a7c5a92c62def11e0805483b8
Evgeny Lopukhin <evg.Lopukhin2011@yandex.ru>
2018-11-16 09:54:49 +0300

    change4

--- portfolio/js.js
+++ portfolio/js.js
@@ -1,3 +1,17 @@
+
+$('.sl').slick({
+prevArrow: '<div class="prev"></div>',
+nextArrow: '<div class="next"></div>',
+   arrows: true,
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+});
+
+ // _____________________
+
 $(document).ready(function() {
    $("a.topLink").click(function() {
       $("html, body").animate({
