d9bff5973c0cfd25ca3974279f44fae5ff57c122
Evgeny Lopukhin <evgenylopukhin@yandex.ru>
2019-07-09 15:01:54 +0300

    a10

--- redsoft/js/main.js
+++ redsoft/js/main.js
@@ -1,18 +1,14 @@
 $(document).ready(function(){
 	
 	/* carousel */
-	$('.header-carousel_js').owlCarousel({
-	    loop: false,
-	    margin: 0,
-	    items: 1,
-	    dots: true,
-	    nav: false,
-	    autoplay: true,
-	    autoplayTimeout: 5000,
-	    animateOut: 'fadeOut',
-	    smartSpeed: 1000
+	$('.header-carousel').slick({
+	  dots: true,
+	  arrows: false,
+	  infinite: true,
+	  speed: 500,
+	  fade: true,
+	  cssEase: 'linear'
 	});
-	$(".owl-dots").wrapAll("<div class='owl-wrap'></div>");
 
 	/* menu */
 	$('.btn-menu').on('click', function() {
