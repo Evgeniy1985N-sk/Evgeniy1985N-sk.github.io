791d998510dbde3296908699240268a294044b56
Evgeny Lopukhin <evgenylopukhin@yandex.ru>
2019-08-01 13:37:16 +0300

    a

---
+++ build/js/main.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+
+	/* preloader */
+	NProgress.start();
+	NProgress.done();
+
+	/* carousel */
+	$('.header-carousel__wrap').slick({
+		dots: true,
+		infinite: false,
+		speed: 300,
+		slidesToShow: 1,
+		arrows: true
+	});
+	$('.header-carousel .slick-dots button:contains("1")').addClass('size_big');
+	$('.header-carousel .slick-dots button:contains("2")').addClass('size_middle');
+
+	/* menu */
+	$('.btn-open').on('click', function() {
+		$('.header-nav').toggleClass('active');
+		$('.btn-open.mob-menu').toggleClass('active');
+	});
+	$('.btn-close.menu').on('click', function() {
+		$('.header-nav').removeClass('active');
+		$('.btn-open.mob-menu').removeClass('active');
+	});
+
+	/* popup */
+	$('.btn-close.request-popup_big').on('click', function() {
+        $.fancybox.close();
+	});
+
+	/* detect browser */
+	if (navigator.userAgent.indexOf('Trident') > 0 ) {
+		$('body').addClass('ie');
+	}
+	if (navigator.userAgent.indexOf('Edge') > 0 ) {
+		$('body').addClass('edge');
+	}
+	if (navigator.userAgent.indexOf('SamsungBrowser') > 0 ) {
+		$('body').addClass('samsung');
+	}
+
+});
+
+window.onload = function () {
+	$('.preloader').addClass('hidden');
+};
