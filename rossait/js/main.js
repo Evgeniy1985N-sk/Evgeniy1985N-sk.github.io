e6c8035dfc0c65351edccb9adfcb583c7f8cadb6
Evgeny Lopukhin <evgenylopukhin@yandex.ru>
2020-02-01 12:08:56 +0300

    правка верстки

--- rossait/js/main.js
+++ rossait/js/main.js
@@ -8,15 +8,15 @@ $(document).ready(function() {
 
 	// carousel =======================================
 
-	$('.demo .carousel').slick({
+	let slider = $('.demo .carousel').slick({
 		dots: false,
 		fade: true,
-		infinite: true,
 		speed: 300,
 		autoplay: true,
 		autoplaySpeed: 3000,
 		lidessToShow: 1,
 		arrows: true,
+		horizontalSwiping: true,
 		responsive: [
 	        {
 			breakpoint: 769,
@@ -29,9 +29,18 @@ $(document).ready(function() {
 
 	// carousel change =======================================
 
-	$('.demo .carousel').on('afterChange', function(event, slick, currentSlide, nextSlide) {
-		$('.demo .switch-item').removeClass('active');
-		$('.demo .switch-item').eq(currentSlide).addClass('active');
+	slider.on('wheel', (function(e) {
+	  e.preventDefault();
+	  if (e.originalEvent.deltaY < 0) {
+	    $(this).slick('slickNext');
+	  } else {
+	    $(this).slick('slickPrev');
+	  }
+	}));
+
+	$('.demo .carousel').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+  		$('.demo .switch-item').removeClass('active');
+		$('.demo .switch-item').eq(nextSlide).addClass('active');
 	});
 
 	// switch =======================================
